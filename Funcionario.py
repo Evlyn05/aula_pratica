09b9da9d52dcdb13a1f83c07ebac1de52fc1537d
Evlyn05 <164870300+Evlyn05@users.noreply.github.com>
2024-07-23 16:57:37 -0300

    Update Funcionario.py
    Organização

--- Funcionario.py
+++ Funcionario.py
@@ -1,26 +1,22 @@
 class Funcionario:
     def __init__(self, nome, salario):
-        self.nome = nome  # Atributo público
-        self._salario = salario  # Atributo protegido
-        self.__cpf = None  # Atributo privado
+        self.nome = nome 
+        self._salario = salario 
+        self.__cpf = None 
 
 
-    # Método para consultar o CPF (atributo privado)
     def get_cpf(self):
         return self.__cpf
 
 
-    # Método para modificar o CPF (atributo privado)
     def set_cpf(self, cpf):
         self.__cpf = cpf
 
 
-    # Método para consultar o salário (atributo protegido)
     def get_salario(self):
         return self._salario
 
 
-    # Método para modificar o salário (atributo protegido)
     def set_salario(self, salario):
         self._salario = salario
 
@@ -31,26 +27,23 @@ class Funcionario:
 
 class Funcionario2:
     def __init__(self, nome, salario):
-        self.nome = nome  # Atributo público
-        self._salario = salario  # Atributo protegido
-        self.__cpf = None  # Atributo privado
+        self.nome = nome 
+        self._salario = salario  
+        self.__cpf = None 
 
 
-    # Método para consultar o CPF (atributo privado)
+ 
     def get_cpf(self):
         return self.__cpf
 
 
-    # Método para modificar o CPF (atributo privado)
     def set_cpf(self, cpf):
         self.__cpf = cpf
 
 
-    # Método para consultar o salário (atributo protegido)
     def get_salario(self):
         return self._salario
 
-    # Método para modificar o salário (atributo protegido)
     def set_salario(self, salario):
         self._salario = salario
 
@@ -63,7 +56,7 @@ class Funcionario2:
 class Engenheiro(Funcionario):
     def __init__(self, nome, salario, especialidade):
         super().__init__(nome, salario)
-        self.especialidade = especialidade  # Atributo público específico da classe derivada
+        self.especialidade = especialidade  
 
 
     def descricao(self):
@@ -76,4 +69,4 @@ class Engenheiro(Funcionario):
 class Gerente(Funcionario):
     def __init__(self, nome, salario, setor):
         super().__init__(nome, salario)
-        self.setor = setor  # Atributo público específico da classe derivada
+        self.setor = setor 
